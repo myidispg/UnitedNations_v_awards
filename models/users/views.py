6e86ddcc8b55a48b5d6e85b944cdede046d26d3e
myidispg <36075176+myidispg@users.noreply.github.com>
2018-07-15 15:38:54 +0530

    Added goslate translation library and changed register errors for hindi end point

--- models/users/views.py
+++ models/users/views.py
@@ -1,3 +1,4 @@
+import goslate
 from flask import Blueprint, request, session, redirect, url_for, render_template
 
 from models.users.user import User
@@ -20,40 +21,13 @@ def login_user():
             return e.message
 
     # this return works if the request method is GET or the login credentials are invalid
-    return render_template('users/login.html')
+    return render_template('forms/login.html')
 
 
 @user_blueprint.route('register', methods=['GET', 'POST'])
 def register_user():
     if request.method == 'POST':
         email = request.form['email']
-        # password = request.form['hashed'] if request.form['hashed'] else None
-        # name = request.form['name'] if request.form['name'] else None
-        # phone_no = request.form['phone_no'] if request.form['phone_no'] else None
-        # gender = request.form['gender'] if request.form['gender'] else None
-        # dob = request.form['dob'] if request.form['dob'] else None
-        # # if request.form['hashed']:
-        #     password = request.form['hashed']
-        # if request.form['name']:
-        #     name = request.form['name']
-        # if request.form['phone_no']:
-        #     phone_no = request.form['phone_no']
-        # if request.form['gender']:
-        #     gender = request.form['gender']
-        # if request.form['dob']:
-        #     dob = request.form['dob']
-        # try:
-        #     password = request.form['hashed']
-        #     name = request.form['name']
-        #     phone_no = request.form['phone_no']
-        #     gender = request.form['gender']
-        #     dob = request.form['dob']
-        # except NameError:
-        #     password = None
-        #     name = None
-        #     phone_no = None
-        #     gender = None
-        #     dob = None
         password = request.form.get('password')
         name = request.form.get('name')
         phone_no = request.form.get('phone_no')
@@ -66,10 +40,40 @@ def register_user():
                 return redirect(url_for('.user_dashboard'))
         except UserErrors.UserError as e:
             return e.message
-        return render_template('users/register.html')
+
+    return render_template('forms/register.html')
+
+
+@user_blueprint.route('/hi/register', methods=['GET', 'POST'])
+def register_user_hindi():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form.get('password')
+        name = request.form.get('name')
+        phone_no = request.form.get('phone_no')
+        gender = request.form.get('gender')
+        dob = request.form.get('dob')
+
+        try:
+            if User.register_user(email, password, name, phone_no, gender, dob):
+                session['email'] = email
+                return redirect(url_for('.user_dashboard_hindi'))
+        except UserErrors.UserError as e:
+            gs = goslate.Goslate()
+            message = gs.translate(e.message, 'hi')
+            return message
+
+    return render_template('forms/register.html')
 
 
 @user_blueprint.route('user-dashboard')
 def user_dashboard():
     email = session['email']
     return "Welcome to your dashboard {}!".format(email)
+
+
+@user_blueprint.route('/hi/user-dashboard')
+def user_dashboard_hindi():
+    email = session['email']
+    return "आपके डैशबोर्ड में आपका स्वागत है {}!".format(email)
+
