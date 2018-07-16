b6735338e9133426ebefbf5cf681b25171fade80
myidispg <36075176+myidispg@users.noreply.github.com>
2018-07-16 14:38:54 +0530

    fixed validation of email format to work with . in mail ids

--- common/utils.py
+++ common/utils.py
@@ -7,7 +7,7 @@ class Utils:
 
     @staticmethod
     def email_is_valid(email):
-        email_address_matcher = re.compile('^[\w-]+@([\w-]+\.)+[\w]+$')
+        email_address_matcher = re.compile('^[\w\.\d]+@([\w-]+\.)+[\w]+$')
         return True if email_address_matcher.match(email) else False
 
     @staticmethod
